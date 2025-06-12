6031325f864b75337cc399e05225e06470d7f40a
DenizShayan <92824050+DenizShayan@users.noreply.github.com>
2025-06-12 12:09:35 +0330

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ export default function Home() {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+
     if (!input.trim()) return;
 
     const newTodo: Todo = {
@@ -63,9 +64,8 @@ export default function Home() {
         {todos.map((todo) => (
           <li
             key={todo.id}
-            className={`flex justify-between items-center mb-2 border-b pb-2 ${
-              todo.done ? "text-gray-400 line-through" : ""
-            }`}
+            className={`flex justify-between items-center mb-2 border-b pb-2 ${todo.done ? "text-gray-400 line-through" : ""
+              }`}
           >
             <span onClick={() => toggleTodo(todo.id)} className="cursor-pointer">
               {todo.text}
